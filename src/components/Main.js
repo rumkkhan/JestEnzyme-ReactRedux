6250a3041ebb1789c082bd083dfd62c4ef5567f8
Ramzan <rumkkhan@gmail.com>
2020-01-23 01:06:27 +0530

    add basic authentication setup

---
+++ src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './Authenticaiton/Header'
+
+
+export default function(props) {
+    console.log(props)
+    return (
+        <div>
+            <Header />
+            hi
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import Root from 'Root';
 import { BrowserRouter , Route } from 'react-router-dom';
 import App from 'components/App'
+import Main from 'components/Main'
 
 
 ReactDOM.render(
   <Root>
     <BrowserRouter>
-      <Route path="/" component={App} />
-    </BrowserRouter>
-    
+      <Route path="/Main" component={(props) => <Main {...props} />} />
+    </BrowserRouter>   
     </Root> 
 , document.querySelector('#root')); 
